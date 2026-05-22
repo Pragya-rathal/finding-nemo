abdaf9d7f336a56afac02f07f6dd47d1322a7be2
Pragya-rathal <pragya.rathal@gmail.com>
2026-05-22 10:45:27 +0530

    Delete models directory

--- models/matcher.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def simple_match(pred_boxes,target_boxes):
-    if pred_boxes.numel()==0 or target_boxes.numel()==0: return torch.empty(0,dtype=torch.long)
-    ious=torch.cdist(pred_boxes,target_boxes)
-    return ious.argmin(dim=1)
--- models/taxonomy_heads.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch import nn
-
-class TaxonomyHeads(nn.Module):
-    def __init__(self,dim:int,order:int,family:int,genus:int,species:int,traits:int):
-        super().__init__()
-        self.order=nn.Linear(dim,order); self.family=nn.Linear(dim,family); self.genus=nn.Linear(dim,genus); self.species=nn.Linear(dim,species); self.trait=nn.Linear(dim,traits)
-    def forward(self,x):
-        return {'order_logits':self.order(x),'family_logits':self.family(x),'genus_logits':self.genus(x),'species_logits':self.species(x),'trait_logits':self.trait(x)}
