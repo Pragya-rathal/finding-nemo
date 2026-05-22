2771653aac5ed662e4aa8dc4724bf69ca18abaa8
Pragya-rathal <pragya.rathal@gmail.com>
2026-05-22 10:46:10 +0530

    Delete utils directory

--- utils/profiling.py
+++
@@ -1,7 +0,0 @@
-import time
-from contextlib import contextmanager
-
-@contextmanager
-def timer(name: str='block'):
-    st=time.time(); yield; et=time.time()
-    print(f'{name}: {et-st:.4f}s')
--- utils/seed.py
+++
@@ -1,8 +0,0 @@
-import os, random, numpy as np, torch
-
-def set_seed(seed: int) -> None:
-    random.seed(seed)
-    np.random.seed(seed)
-    os.environ['PYTHONHASHSEED']=str(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
