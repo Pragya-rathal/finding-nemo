3f7b03552ad5751194607ffda1e0722931ac9b93
Pragya-rathal <pragya.rathal@gmail.com>
2026-05-22 10:45:13 +0530

    Delete datasets directory

--- datasets/coco_parser.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-import json
-
-def load_coco(path:str):
-    with open(path,'r',encoding='utf-8') as f: return json.load(f)
--- datasets/transforms.py
+++
@@ -1,7 +0,0 @@
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-
-def build_transforms(size=640,train=True):
-    if train:
-        return A.Compose([A.Resize(size,size),A.RandomCrop(size,size,p=0.5),A.HorizontalFlip(p=0.5),A.Blur(p=0.2),A.CLAHE(p=0.2),A.ColorJitter(p=0.3),A.Normalize(),ToTensorV2()],bbox_params=A.BboxParams(format='pascal_voc',label_fields=['labels']))
-    return A.Compose([A.Resize(size,size),A.Normalize(),ToTensorV2()],bbox_params=A.BboxParams(format='pascal_voc',label_fields=['labels']))
